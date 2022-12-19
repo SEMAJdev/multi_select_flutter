c8c4e73131a15f2468806a463c189be370b72dff
Sathaporn Moonnikorn <100668420+SEMAJdev@users.noreply.github.com>
2022-12-19 10:25:07 +0700

    add Widget? imageWidget

--- lib/util/multi_select_item.dart
+++ lib/util/multi_select_item.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/material.dart';
 /// A model class used to represent a selectable item.
 class MultiSelectItem<T> {
   final T value;
   final String label;
+  final Widget? imageWidget;
   bool selected = false;
 
-  MultiSelectItem(this.value, this.label);
+  MultiSelectItem(this.value, this.label, this.imageWidget);
 }
